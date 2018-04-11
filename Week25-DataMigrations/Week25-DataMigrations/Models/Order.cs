51ce9fec5f3d6b26a3ebf3d3753d5efc463cb267
maxamillls <morrm046@gmail.com>
2018-04-11 14:43:21 +0100

    Redid the code which i was stuck on at Uni

---
+++ Week25-DataMigrations/Week25-DataMigrations/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week25_DataMigrations.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateDispatched { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+
+    }
+}
